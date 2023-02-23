cb7cb1e06439c440152908f18a6558b11a652e38
Bryan Runnels <bryan.d.runnels@gmail.com>
2023-02-23 15:00:25 -0700

    Third

--- src/components/SingleDog.jsx
+++ src/components/SingleDog.jsx
@@ -3,12 +3,19 @@ import { useParams } from "react-router-dom";
 const SingleDog = (props) => {
     const { idNumber } = useParams();
     
-    // console.log(idNumber)
-    const selectedDog = props.dogProps.id
-    console.log(selectedDog)
+    
+    const selectedDogArray = props.dogProps.filter((singleDog) => {
+                
+            return idNumber == singleDog.id
+})
+
+    const selectedDog = selectedDogArray[0]
+
+    console.log(selectedDogArray)
     return (
         <div>
-            <h3> Name: {selectedDog} </h3>
+            <h3> Name: {selectedDog.name} </h3>
+            
         </div>
     )
 }
